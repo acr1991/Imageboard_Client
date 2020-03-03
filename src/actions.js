f5143f3979c318c53e0e63a6dce2ec77c31494be
acr1991 <acr1991@live.nl>
2020-03-03 12:25:50 +0100

    minor iterations

--- src/actions.js
+++ src/actions.js
@@ -1,12 +1,7 @@
 import request from "superagent";
-export const ALL_IMAGES = "ALL_IMAGES";
+
 const baseUrl = "http://localhost:4000";
-function allImages(payload) {
-  return {
-    type: ALL_IMAGES,
-    payload
-  };
-}
+
 export const getImages = () => (dispatch, getState) => {
   const state = getState();
   const { images } = state;
@@ -19,16 +14,23 @@ export const getImages = () => (dispatch, getState) => {
       .catch(console.error);
   }
 };
-export const NEW_IMAGE = "NEW_IMAGE";
-function newImage(payload) {
-  return {
-    type: NEW_IMAGE,
-    payload
-  };
-}
-export const createImage = data => dispatch => {
+
+export const createUser = data => (dispatch, getState) => {
+  console.log("HERE");
+  request
+    .post(`${baseUrl}/user`)
+    .send(data)
+    .then(response => {
+      dispatch(newUser(response));
+    });
+};
+
+export const createImage = data => (dispatch, getState) => {
+  const state = getState();
+  const { user } = state;
   request
     .post(`${baseUrl}/image`)
+    .set("Authorization", `Bearer ${user.jwt}`)
     .send(data)
     .then(response => {
       const action = newImage(response.body);
@@ -36,3 +38,43 @@ export const createImage = data => dispatch => {
     })
     .catch(console.error);
 };
+
+export const login = data => dispatch => {
+  request
+    .post(`${baseUrl}/login`)
+    .send(data)
+    .then(response => {
+      console.log("RESPONSE ", response);
+      const action = newLogin(response.body);
+      dispatch(action);
+    })
+    .catch(err => console.log("from err: ", err));
+};
+
+function newUser(data) {
+  return {
+    type: "NEW_USER",
+    payload: data
+  };
+}
+
+function newLogin(payload) {
+  return {
+    type: "NEW_LOGIN",
+    payload: payload
+  };
+}
+
+function newImage(image) {
+  return {
+    type: "NEW_IMAGE",
+    payload: image
+  };
+}
+
+function allImages(images) {
+  return {
+    type: "ALL_IMAGES",
+    payload: images
+  };
+}
